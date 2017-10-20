ee528941bc708e61cf2216a15778379a809e8be6
ilix <src@ilix.se>
2017-10-20 15:26:06 +0200

    Created a small API.

---
+++ index.js
@@ -0,0 +1,16 @@
+const restify = require('restify')
+
+const app = restify.createServer({})
+app.pre(restify.pre.sanitizePath())
+// app.use(restify.CORS())
+// app.use(restify.bodyParser())
+// app.use(restify.queryParser())
+
+app.get('/', (req, res, next) => {
+  res.end(`meow =^_^=`)
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log('listening on', port)
+})
